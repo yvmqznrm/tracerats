2575591fe8d5317f3281a8a6a3bf0426968b5d84
DarioACG <112749320+DarioACG@users.noreply.github.com>
2023-07-25 17:00:21 +0300

    Navbar-test-2

--- Website/scripts/navbar.js
+++ Website/scripts/navbar.js
@@ -1,5 +1,5 @@
 // Thank you to https://stackoverflow.com/users/2840531/mendi-barel from https://stackoverflow.com/questions/31954089/how-can-i-reuse-a-navigation-bar-on-multiple-pages for this!
-fetch('https://tracerats.org/reused_elements/navbar.html')
+fetch('reused_elements/navbar.html')
 .then(res => res.text())
 .then(text => {
     let oldelem = document.querySelector("script#replace_with_navbar");
